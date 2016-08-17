81b4d935bc2034d580e1daa9207c2e93a8404234
Lauri Arimo <lauri@arimo.net>
2016-08-17 22:13:30 +0300

    add some debug

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get('/', function (req, res) {
   res.send('Taco de kick!');
 });
 
+console.log('trying to listen on: ', process.env.PORT || 3000)
+
 var server = app.listen(process.env.PORT || 3000, function() {
 
     var host = server.address().address;
