33f905b84ae5bdf5da795385dd0b046d41d2d4ab
Lauri Arimo <lauri@arimo.net>
2016-08-17 22:18:23 +0300

    strip unix: prefix from domain socket

--- app.js
+++ app.js
@@ -6,12 +6,4 @@ app.get('/', function (req, res) {
   res.send('Taco de kick!');
 });
 
-console.log('trying to listen on: ', process.env.PORT || 3000)
-
-var server = app.listen(process.env.PORT || 3000, function() {
-
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-});
+var server = app.listen((process.env.PORT || 3000).replace('unix:',''));
