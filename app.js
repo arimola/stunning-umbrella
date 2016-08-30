1c74a4d5f1a4becd9bf137eba0b288c12d4ab727
Lauri Arimo <lauri.arimo@ixonos.com>
2016-08-30 17:28:26 +0300

    consider deploying locale specific versions of the app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Niklas!!!');
+  res.send('Heippa maailma!!!');
 });
 
 var server = app.listen((process.env.PORT || '3000').replace('unix:',''));
